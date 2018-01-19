532400ddd292b467fa4ec296aad6196f9a9bcefd
Anthony <anthonyjlower@gmail.com>
2018-01-19 14:48:32 -0600

    Created file structre, express server is running, mongodb is running

---
+++ db/db.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb://localhost:27017/movielist", {
+	// useMongoClient: true
+});
+
+mongoose.connection.on("connected", () => {
+	console.log("Mongodb is connected")
+})
+
+mongoose.connection.on("disconnected", () => {
+	console.log("Mongodb is disconnected")
+})
+
+mongoose.connection.on("error", (error) => {
+	console.log("Error connecting -- ", error)
+})
--- server.js
+++ server.js
@@ -1,1 +1,35 @@
-console.log("something quick")
+const express = require('express');
+const app = express();
+const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
+const session = require("express-session");
+
+require("./db/db.js");
+
+
+
+/*
+Middleware
+*/
+app.use(bodyParser.urlencoded({
+	extended: false
+}));
+
+app.use(session({
+	secret: "Holder String",
+	resave: false,
+	saveUninitialized: false
+}))
+
+app.use(methodOverride('_method'))
+
+app.use(express.static("public"));
+
+
+
+
+
+
+app.listen(3000, () => {
+	console.log("Listening on port 3000");
+})
