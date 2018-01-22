5c0aecc3aa67f57199bd3e8fa267212c090f2cc6
Somers Matthews <somersbmatthews@uchicago.edu>
2018-01-22 12:15:38 -0600

    the title from search modal works

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -3,41 +3,73 @@ const request = require('request');
 const router = express.Router();
 const apiKey = "c6ba51285da546e27050e39e5bf072be";
 
-const options = { method: 'GET',
+
+
+const discoverOptions = { method: 'GET',
 	  url: 'https://api.themoviedb.org/3/discover/movie',
 	  qs: 
-	   { primary_release_year: '2017',
-	   	 with_genres: 18,
-	   	 with_cast: 
-	   	 "vote_average.gte": 7,
+	   { primary_release_year: '',
+	   	 with_genres: "",
+	   	 with_cast: "",
+	   	 "vote_average.gte": "",
 	   	 page: '1',
 	     include_video: 'false',
 	     include_adult: 'false',
 	     sort_by: 'popularity.desc',
 	     language: 'en-US',
-	     api_key: 'c6ba51285da546e27050e39e5bf072be' },
+	     api_key: apiKey },
 	     body: '{}' 
 	 };
+const searchMovies = { method: 'GET',
+  url: 'https://api.themoviedb.org/3/search/movie',
+  qs: 
+   { include_adult: 'false',
+     page: '1',
+     query: '',
+     language: 'en-US',
+     api_key: apiKey },
+  body: '{}'
+};
+
 
 
 router.get('/results', (req,res) =>{
-	console.log(options);
+
 	// res.send('check the console');
-	request(options, (error, response, body) => {
-	  if (error) throw new Error(error);
-			const bodyJSON = JSON.parse(body)
-			// console.log(bodyJSON)
-	  	res.render('movies/results.ejs', {
-				body: bodyJSON
+	if(searchMovies.qs.query!=''){
+		request(searchMovies, function (error, response, body) {
+	    	if (error) throw new Error(error);
+	    	const bodyJSON = JSON.parse(body)
+	    	console.log('if there is a title enterered the body is: ', bodyJSON)
+	        	res.render('movies/results.ejs', {
+					body: bodyJSON
+		   		 });
 	    });
-	});
+
+	} else {
+
+
+	    request(discoverOptions, (error, response, body) => {
+		    if (error) throw new Error(error);
+				const bodyJSON = JSON.parse(body)
+				console.log('if there is a title NOT enterered the body is: ', bodyJSON)
+				// console.log(bodyJSON)
+		  			res.render('movies/results.ejs', {
+						body: bodyJSON
+		  		    });
+	    });
+	}
 })
 
 router.post('/results', (req, res) => {
-	// console.log(req.body.genre)
-	options.qs.with_genres = req.body.genre;
-	options.qs.primary_release_year = req.body.releaseYear;
-	options.qs["vote_average.gte"] = req.body.minRating;
+
+	
+	searchMovies.qs.query = req.body.title
+	console.log("the title requested is ", req.body.title)
+	// set all discover options for api discover method based on what search modal passes
+	discoverOptions.qs.with_genres = req.body.genre;
+	discoverOptions.qs.primary_release_year = req.body.releaseYear;
+	discoverOptions.qs["vote_average.gte"] = req.body.minRating;
 	res.redirect("/movies/results")
 })
 
---
+++ models/sampleData.js
@@ -0,0 +1,423 @@
+
+
+const movieData = 
+{
+  "page": 1,
+  "total_results": 342279,
+  "total_pages": 17114,
+  "results": [
+    {
+      "vote_count": 5901,
+      "id": 346364,
+      "video": false,
+      "vote_average": 7.1,
+      "title": "It",
+      "popularity": 710.916995,
+      "poster_path": "/9E2y5Q7WlCVNEhP5GiVTjhEhx1o.jpg",
+      "original_language": "en",
+      "original_title": "It",
+      "genre_ids": [
+        18,
+        27,
+        53
+      ],
+      "backdrop_path": "/tcheoA2nPATCm2vvXw2hVQoaEFD.jpg",
+      "adult": false,
+      "overview": "In a small town in Maine, seven children known as The Losers Club come face to face with life problems, bullies and a monster that takes the shape of a clown called Pennywise.",
+      "release_date": "2017-09-05"
+    },
+    {
+      "vote_count": 1562,
+      "id": 353486,
+      "video": false,
+      "vote_average": 6.4,
+      "title": "Jumanji: Welcome to the Jungle",
+      "popularity": 633.341715,
+      "poster_path": "/bXrZ5iHBEjH7WMidbUDQ0U2xbmr.jpg",
+      "original_language": "en",
+      "original_title": "Jumanji: Welcome to the Jungle",
+      "genre_ids": [
+        28,
+        12,
+        35,
+        10751
+      ],
+      "backdrop_path": "/cpz070zEKbPGXzCWuQsNt42PqXY.jpg",
+      "adult": false,
+      "overview": "The tables are turned as four teenagers are sucked into Jumanji's world - pitted against rhinos, black mambas and an endless variety of jungle traps and puzzles. To survive, they'll play as characters from the game.",
+      "release_date": "2017-12-08"
+    },
+    {
+      "vote_count": 3242,
+      "id": 181808,
+      "video": false,
+      "vote_average": 7.2,
+      "title": "Star Wars: The Last Jedi",
+      "popularity": 469.077234,
+      "poster_path": "/xGWVjewoXnJhvxKW619cMzppJDQ.jpg",
+      "original_language": "en",
+      "original_title": "Star Wars: The Last Jedi",
+      "genre_ids": [
+        28,
+        12,
+        14,
+        878
+      ],
+      "backdrop_path": "/5Iw7zQTHVRBOYpA0V6z0yypOPZh.jpg",
+      "adult": false,
+      "overview": "Rey develops her newly discovered abilities with the guidance of Luke Skywalker, who is unsettled by the strength of her powers. Meanwhile, the Resistance prepares to do battle with the First Order.",
+      "release_date": "2017-12-13"
+    },
+    {
+      "vote_count": 314,
+      "id": 460793,
+      "video": false,
+      "vote_average": 5.9,
+      "title": "Olaf's Frozen Adventure",
+      "popularity": 369.513244,
+      "poster_path": "/47pLZ1gr63WaciDfHCpmoiXJlVr.jpg",
+      "original_language": "en",
+      "original_title": "Olaf's Frozen Adventure",
+      "genre_ids": [
+        12,
+        16,
+        35,
+        10751,
+        14,
+        10402
+      ],
+      "backdrop_path": "/woCxv9kua5uqtRNaiWUBDzrgZnH.jpg",
+      "adult": false,
+      "overview": "Olaf is on a mission to harness the best holiday traditions for Anna, Elsa, and Kristoff.",
+      "release_date": "2017-10-27"
+    },
+    {
+      "vote_count": 627,
+      "id": 316029,
+      "video": false,
+      "vote_average": 8,
+      "title": "The Greatest Showman",
+      "popularity": 298.452388,
+      "poster_path": "/dfhztJRiElqmYW4kpvjYe1gENsD.jpg",
+      "original_language": "en",
+      "original_title": "The Greatest Showman",
+      "genre_ids": [
+        18,
+        10751,
+        10402,
+        10749
+      ],
+      "backdrop_path": "/zpq404Sk7qQ7N4x3xOeNgp74GtU.jpg",
+      "adult": false,
+      "overview": "The story of American showman P.T. Barnum, founder of the circus that became the famous traveling Ringling Bros. and Barnum & Bailey Circus.",
+      "release_date": "2017-12-20"
+    },
+    {
+      "vote_count": 6410,
+      "id": 245891,
+      "video": false,
+      "vote_average": 7,
+      "title": "John Wick",
+      "popularity": 279.500309,
+      "poster_path": "/5vHssUeVe25bMrof1HyaPyWgaP.jpg",
+      "original_language": "en",
+      "original_title": "John Wick",
+      "genre_ids": [
+        28,
+        53
+      ],
+      "backdrop_path": "/umC04Cozevu8nn3JTDJ1pc7PVTn.jpg",
+      "adult": false,
+      "overview": "Ex-hitman John Wick comes out of retirement to track down the gangsters that took everything from him.",
+      "release_date": "2014-10-22"
+    },
+    {
+      "vote_count": 1301,
+      "id": 354912,
+      "video": false,
+      "vote_average": 7.6,
+      "title": "Coco",
+      "popularity": 276.876471,
+      "poster_path": "/eKi8dIrr8voobbaGzDpe8w0PVbC.jpg",
+      "original_language": "en",
+      "original_title": "Coco",
+      "genre_ids": [
+        12,
+        16,
+        10751,
+        14
+      ],
+      "backdrop_path": "/askg3SMvhqEl4OL52YuvdtY40Yb.jpg",
+      "adult": false,
+      "overview": "Despite his family’s baffling generations-old ban on music, Miguel dreams of becoming an accomplished musician like his idol, Ernesto de la Cruz. Desperate to prove his talent, Miguel finds himself in the stunning and colorful Land of the Dead following a mysterious chain of events. Along the way, he meets charming trickster Hector, and together, they set off on an extraordinary journey to unlock the real story behind Miguel's family history.",
+      "release_date": "2017-10-27"
+    },
+    {
+      "vote_count": 4346,
+      "id": 374720,
+      "video": false,
+      "vote_average": 7.4,
+      "title": "Dunkirk",
+      "popularity": 273.456846,
+      "poster_path": "/bOXBV303Fgkzn2K4FeKDc0O31q4.jpg",
+      "original_language": "en",
+      "original_title": "Dunkirk",
+      "genre_ids": [
+        28,
+        18,
+        36,
+        53,
+        10752
+      ],
+      "backdrop_path": "/4yjJNAgXBmzxpS6sogj4ftwd270.jpg",
+      "adult": false,
+      "overview": "The story of the miraculous evacuation of Allied soldiers from Belgium, Britain, Canada and France, who were cut off and surrounded by the German army from the beaches and harbour of Dunkirk between May 26th and June 4th 1940 during World War II.",
+      "release_date": "2017-07-19"
+    },
+    {
+      "vote_count": 5301,
+      "id": 211672,
+      "video": false,
+      "vote_average": 6.4,
+      "title": "Minions",
+      "popularity": 248.280834,
+      "poster_path": "/q0R4crx2SehcEEQEkYObktdeFy.jpg",
+      "original_language": "en",
+      "original_title": "Minions",
+      "genre_ids": [
+        10751,
+        16,
+        12,
+        35
+      ],
+      "backdrop_path": "/qLmdjn2fv0FV2Mh4NBzMArdA0Uu.jpg",
+      "adult": false,
+      "overview": "Minions Stuart, Kevin and Bob are recruited by Scarlet Overkill, a super-villain who, alongside her inventor husband Herb, hatches a plot to take over the world.",
+      "release_date": "2015-06-17"
+    },
+    {
+      "vote_count": 240,
+      "id": 271404,
+      "video": false,
+      "vote_average": 4.8,
+      "title": "Beyond Skyline",
+      "popularity": 229.10496,
+      "poster_path": "/lQUJZIm2nQj4mIsm0sUcznhMdpD.jpg",
+      "original_language": "en",
+      "original_title": "Beyond Skyline",
+      "genre_ids": [
+        28,
+        878
+      ],
+      "backdrop_path": "/m5jvdwlI24XpFDSX79o97z76jis.jpg",
+      "adult": false,
+      "overview": "A tough-as-nails detective embarks on a relentless pursuit to free his son from a nightmarish alien warship.",
+      "release_date": "2017-10-20"
+    },
+    {
+      "vote_count": 2661,
+      "id": 335984,
+      "video": false,
+      "vote_average": 7.4,
+      "title": "Blade Runner 2049",
+      "popularity": 216.226391,
+      "poster_path": "/gajva2L0rPYkEWjzgFlBXCAVBE5.jpg",
+      "original_language": "en",
+      "original_title": "Blade Runner 2049",
+      "genre_ids": [
+        9648,
+        878,
+        53
+      ],
+      "backdrop_path": "/m7ynwXIvSnhxQPR6pOICrC0L2sO.jpg",
+      "adult": false,
+      "overview": "Thirty years after the events of the first film, a new blade runner, LAPD Officer K, unearths a long-buried secret that has the potential to plunge what's left of society into chaos. K's discovery leads him on a quest to find Rick Deckard, a former LAPD blade runner who has been missing for 30 years.",
+      "release_date": "2017-10-04"
+    },
+    {
+      "vote_count": 197,
+      "id": 347882,
+      "video": false,
+      "vote_average": 5.4,
+      "title": "Sleight",
+      "popularity": 215.116477,
+      "poster_path": "/wridRvGxDqGldhzAIh3IcZhHT5F.jpg",
+      "original_language": "en",
+      "original_title": "Sleight",
+      "genre_ids": [
+        18,
+        53,
+        28,
+        878
+      ],
+      "backdrop_path": "/2SEgJ0mHJ7TSdVDbkGU061tR33K.jpg",
+      "adult": false,
+      "overview": "A young street magician is left to take care of his little sister after his mother's passing and turns to drug dealing in the Los Angeles party scene to keep a roof over their heads. When he gets into trouble with his supplier, his sister is kidnapped and he is forced to rely on both his sleight of hand and brilliant mind to save her.",
+      "release_date": "2017-04-28"
+    },
+    {
+      "vote_count": 145,
+      "id": 406563,
+      "video": false,
+      "vote_average": 6.2,
+      "title": "Insidious: The Last Key",
+      "popularity": 214.322799,
+      "poster_path": "/nb9fc9INMg8kQ8L7sE7XTNsZnUX.jpg",
+      "original_language": "en",
+      "original_title": "Insidious: The Last Key",
+      "genre_ids": [
+        9648,
+        53,
+        27
+      ],
+      "backdrop_path": "/auSRjQyVLw212IjATAbO3KOyxZg.jpg",
+      "adult": false,
+      "overview": "Parapsychologist Elise Rainier and her team travel to Five Keys, N.M., to investigate a man's claim of a haunting. Terror soon strikes when Rainier realizes that the house he lives in was her family's old home.",
+      "release_date": "2018-01-03"
+    },
+    {
+      "vote_count": 6183,
+      "id": 283995,
+      "video": false,
+      "vote_average": 7.6,
+      "title": "Guardians of the Galaxy Vol. 2",
+      "popularity": 195.621755,
+      "poster_path": "/y4MBh0EjBlMuOzv9axM4qJlmhzz.jpg",
+      "original_language": "en",
+      "original_title": "Guardians of the Galaxy Vol. 2",
+      "genre_ids": [
+        28,
+        12,
+        35,
+        878
+      ],
+      "backdrop_path": "/aJn9XeesqsrSLKcHfHP4u5985hn.jpg",
+      "adult": false,
+      "overview": "The Guardians must fight to keep their newfound family together as they unravel the mysteries of Peter Quill's true parentage.",
+      "release_date": "2017-04-19"
+    },
+    {
+      "vote_count": 304,
+      "id": 353616,
+      "video": false,
+      "vote_average": 6.6,
+      "title": "Pitch Perfect 3",
+      "popularity": 183.050127,
+      "poster_path": "/nZgq5ioz7mC39IALmsua6nrPNek.jpg",
+      "original_language": "en",
+      "original_title": "Pitch Perfect 3",
+      "genre_ids": [
+        35
+      ],
+      "backdrop_path": "/1qIzlhLGPSm6TxlvXBWe0Q5er7O.jpg",
+      "adult": false,
+      "overview": "After the highs of winning the world championships, the Bellas find themselves split apart and discovering there aren't job prospects for making music with your mouth. But when they get the chance to reunite for an overseas USO tour, this group of awesome nerds will come together to make some music, and some questionable decisions, one last time.",
+      "release_date": "2017-12-21"
+    },
+    {
+      "vote_count": 3349,
+      "id": 8844,
+      "video": false,
+      "vote_average": 6.9,
+      "title": "Jumanji",
+      "popularity": 180.730738,
+      "poster_path": "/8wBKXZNod4frLZjAKSDuAcQ2dEU.jpg",
+      "original_language": "en",
+      "original_title": "Jumanji",
+      "genre_ids": [
+        12,
+        14,
+        10751
+      ],
+      "backdrop_path": "/7k4zEgUZbzMHawDaMc9yIkmY1qR.jpg",
+      "adult": false,
+      "overview": "When siblings Judy and Peter discover an enchanted board game that opens the door to a magical world, they unwittingly invite Alan -- an adult who's been trapped inside the game for 26 years -- into their living room. Alan's only hope for freedom is to finish the game, which proves risky as all three find themselves running from giant rhinoceroses, evil monkeys and other terrifying creatures.",
+      "release_date": "1995-12-15"
+    },
+    {
+      "vote_count": 96,
+      "id": 446354,
+      "video": false,
+      "vote_average": 6.6,
+      "title": "The Post",
+      "popularity": 179.770552,
+      "poster_path": "/qyRwj5VvuTRdJ76o2grP93grNxt.jpg",
+      "original_language": "en",
+      "original_title": "The Post",
+      "genre_ids": [
+        18,
+        36
+      ],
+      "backdrop_path": "/8sb4aBST28vN3rBz704XJczS0Ld.jpg",
+      "adult": false,
+      "overview": "A cover-up that spanned four U.S. Presidents pushed the country's first female newspaper publisher and a hard-driving editor to join an unprecedented battle between journalist and government. Inspired by true events.",
+      "release_date": "2017-12-22"
+    },
+    {
+      "vote_count": 6540,
+      "id": 321612,
+      "video": false,
+      "vote_average": 6.8,
+      "title": "Beauty and the Beast",
+      "popularity": 178.504646,
+      "poster_path": "/tWqifoYuwLETmmasnGHO7xBjEtt.jpg",
+      "original_language": "en",
+      "original_title": "Beauty and the Beast",
+      "genre_ids": [
+        10751,
+        14,
+        10749
+      ],
+      "backdrop_path": "/6aUWe0GSl69wMTSWWexsorMIvwU.jpg",
+      "adult": false,
+      "overview": "A live-action adaptation of Disney's version of the classic tale of a cursed prince and a beautiful young woman who helps him break the spell.",
+      "release_date": "2017-03-16"
+    },
+    {
+      "vote_count": 364,
+      "id": 399055,
+      "video": false,
+      "vote_average": 7.2,
+      "title": "The Shape of Water",
+      "popularity": 176.249855,
+      "poster_path": "/iLYLADGA5oKGM92Ns1j9CDgk3iI.jpg",
+      "original_language": "en",
+      "original_title": "The Shape of Water",
+      "genre_ids": [
+        12,
+        18,
+        14,
+        27,
+        10749,
+        53
+      ],
+      "backdrop_path": "/bAISaVhsaoyyQMZ55cpTwCdzek4.jpg",
+      "adult": false,
+      "overview": "An other-worldly story, set against the backdrop of Cold War era America circa 1962, where a mute janitor working at a lab falls in love with an amphibious man being held captive there and devises a plan to help him escape.",
+      "release_date": "2017-12-08"
+    },
+    {
+      "vote_count": 6160,
+      "id": 198663,
+      "video": false,
+      "vote_average": 7,
+      "title": "The Maze Runner",
+      "popularity": 165.603456,
+      "poster_path": "/coss7RgL0NH6g4fC2s5atvf3dFO.jpg",
+      "original_language": "en",
+      "original_title": "The Maze Runner",
+      "genre_ids": [
+        28,
+        9648,
+        878,
+        53
+      ],
+      "backdrop_path": "/lkOZcsXcOLZYeJ2YxJd3vSldvU4.jpg",
+      "adult": false,
+      "overview": "Set in a post-apocalyptic world, young Thomas is deposited in a community of boys after his memory is erased, soon learning they're all trapped in a maze that will require him to join forces with fellow “runners” for a shot at escape.",
+      "release_date": "2014-09-10"
+    }
+  ]
+}
+
+module.exports = movieData;
