2539c3392c590b18a48c9647c1911717f973f0a2
Sierra Moore <sierra.v.moore@gmail.com>
2018-01-24 11:08:45 -0600

    working on making it more efficent

--- public/js/app.js
+++ public/js/app.js
@@ -5,57 +5,126 @@ const $submitSearch = $('#submitSearch');
 // so $modal is An array
 const modal = $('.modal')[0];
 
-const openModal = () => {
-    $(modal).css('display', 'block');
-};
-
-const closeModal = () => {
-    $(modal).css('display', 'none');
-};
+// switch that signifies modal is displayed
+let modalDisplayed = false;
 
 
-$(searchIcon).on('click', openModal);
 
-$submitSearch.on('click', closeModal);
-
-
-//parent
-console.log(modal)
 
 
 //form
-let form = $('.modalForm').children()
-console.log(form.prevUntil("button"))
 
-console.log(form.each)
+// console.log(form.prevUntil("button"))
+//
+// console.log(form.each)
+//
+// let inputs = document.querySelectorAll('form')
+//
+//
+// console.log(typeof modal.childNodes)
+//
+// $("input").each(function () {
+//     console.log("each input")
+// })
+//
+// console.log($.inArray('input', form, 3));
 
-let inputs = document.querySelectorAll('form')
-console.log(inputs)
 
-console.log(typeof modal.childNodes)
+const openModal = () => {
+    //alert("openModal");
+    $(modal).css('display', 'block');
+    modalDisplayed = true;
+};
 
-$("input").each(function (index) {
-    console.log("each input")
-})
+$(searchIcon).on('click', openModal);
 
+const closeModal = () => {
+    $(modal).css('display', 'none');
+    modalDisplayed = false;
+};
+
+$submitSearch.on('click', closeModal);
 
 // anywhere clicked
 $('body').on('click', function (event) {
+    console.log(event.target);
 
-    // thats not the modal or search button
-    // if the click is on a modal or anything inside the modal
+    console.log(formElements)
 
+    if ($(modal).css('display') === 'block') {
+        let formElements = $('.modalForm').children();
 
+        const clickedOnSearch = event.target === searchIcon;
+        const clickedOnModal = event.target === modal; //evaluates to boolean and saved in variable
+        const clickedOnModalChildren = formElements(event.target);
 
-        if (event.target === modal || form.each === event.target || event.target === searchIcon) {
-            // do noting
-
-        } else {
+        if (!clickedOnSearch && !clickedOnModal && !clickedOnModalChildren){
             closeModal();
         }
-
+    }
 });
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// if event.currentTarget is a child of modal
+// if modalDisplayed use boolean above
+// hide it
+// if modal is the curre
+// console.log(modal.children)
+//
+// if(modal.children === event.currentTarget.children || event.currentTarget === $submitSearch){
+//     $(modal).css('display', 'block');
+//     modalDisplayed = true;
+// }else{
+//     $(modal).css('display', 'none');
+//     modalDisplayed = false;
+// }
+//
+// console.log(modal)
+
+
+
+
+
+// console.log( form.length)
+// //console.log($(form).children)
+//
+// for(let i=0; i < form.length; i++) {
+//     if (form[i] !== event.currentTarget) {
+//         closeModal()
+//     }
+//
+//     if (event.target === modal || event.target === searchIcon) {
+//         /* ||  form[0] === event.target || form[1] === event.target || form[2] === event.target || form[3] === event.target || form[4] === event.target || form[5] === event.target  */
+//         // do noting
+//
+//     } else {
+//         closeModal();
+//     }
+// }
